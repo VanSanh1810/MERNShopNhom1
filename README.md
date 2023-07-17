d9ef9d5f529b97edb76ad3f05dee8d4e3c1e5911
sanh nguyen <sanhnguyen734@gmail.com>
2023-07-17 21:10:00 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### eCommerce using MERN Stack, React, Node, Express, MonogoDB, Redux Toolkit & Tailwind CSS
+## Doc này đã có cải tiến :)))
---
+++ backend/data/products.js
@@ -0,0 +1,31 @@
+const products = [
+    {
+        title: 'Airpods Wireless Bluetooth Headphones',
+        price: 0,
+        discount: 0,
+        stock: 0,
+        category: 'Electronics',
+        colors: [{
+            colors: {
+              red: '#FF0000',
+              blue: '#0000FF',
+              green: '#00FF00'
+            }
+          }],
+        sizes: [{
+            colors: {
+              red: '#FF0000',
+              blue: '#0000FF',
+              green: '#00FF00'
+            }
+          }],
+        image1: "1b4b9482-85bc-4239-85ab-2bb5951040e6",
+        image2: "1b4b9482-85bc-4239-85ab-2bb5951040e6",
+        image3: "1b4b9482-85bc-4239-85ab-2bb5951040e6",
+        description:
+            'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        reviews: [],
+    },
+];
+
+export default products;
